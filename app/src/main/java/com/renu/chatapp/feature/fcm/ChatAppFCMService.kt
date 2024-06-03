0493f70421f9ceb316b8c2df7a934d05147b11c7
renuswami <renuswami2001@gmail.com>
2024-06-03 20:33:41 +0530

    fcm : handle fcm message manually

--- app/src/main/java/com/renu/chatapp/feature/fcm/ChatAppFCMService.kt
+++ app/src/main/java/com/renu/chatapp/feature/fcm/ChatAppFCMService.kt
@@ -1,5 +1,6 @@
 package com.renu.chatapp.feature.fcm
 
+import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.renu.chatapp.MainActivity
@@ -10,11 +11,20 @@ class ChatAppFCMService: FirebaseMessagingService() {
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
 
+        val data = message.data
+        logData(data)
+
         val notificationParams = message.notification ?: return
         val title = notificationParams.title ?: return
         val body = notificationParams.body ?: return
 
         showNotification(title, body)
+    }
+
+    private fun logData(data: Map<String, String>) {
+        data.forEach { (key, value) ->
+            Log.i("ChatAppDebug","message.data recived : $key : $value")
+        }
     }
 
     private fun showNotification(title: String, body: String) {
