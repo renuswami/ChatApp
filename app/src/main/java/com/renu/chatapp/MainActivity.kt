ec2951c109adea8ba6f5c761290694aacc30a403
renuswami <renuswami2001@gmail.com>
2024-06-03 20:17:14 +0530

    fcm : subscribe to a topic

--- app/src/main/java/com/renu/chatapp/MainActivity.kt
+++ app/src/main/java/com/renu/chatapp/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.renu.chatapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.google.firebase.Firebase
+import com.google.firebase.messaging.messaging
 import com.renu.chatapp.temp.Scripts
 import com.renu.chatapp.ui.ChatAppNavHost
 import com.renu.chatapp.ui.theme.ChatAppTheme
 import com.streamliners.base.BaseActivity
 import com.streamliners.base.uiEvent.UiEventDialogs
+import kotlinx.coroutines.tasks.await
 
 class MainActivity : BaseActivity() {
 
@@ -29,6 +33,11 @@ class MainActivity : BaseActivity() {
                 }
             }
         }
+
+        /* execute {
+            val token = Firebase.messaging.token.await()
+            Log.i("ChatAppDebug", "FCM token: $token")
+        }*/
         // runScripts()
     }
 
--- app/src/main/java/com/renu/chatapp/feature/fcm/ChatAppFCMService.kt
+++ app/src/main/java/com/renu/chatapp/feature/fcm/ChatAppFCMService.kt
@@ -1,6 +1,28 @@
 package com.renu.chatapp.feature.fcm
 
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.renu.chatapp.MainActivity
+import com.streamliners.helpers.NotificationHelper
 
 class ChatAppFCMService: FirebaseMessagingService() {
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+
+        val notificationParams = message.notification ?: return
+        val title = notificationParams.title ?: return
+        val body = notificationParams.body ?: return
+
+        showNotification(title, body)
+    }
+
+    private fun showNotification(title: String, body: String) {
+        NotificationHelper(this)
+            .showNotification(
+                title = title,
+                body = body,
+                pendingIntentActivity = MainActivity::class.java
+            )
+        }
 }
